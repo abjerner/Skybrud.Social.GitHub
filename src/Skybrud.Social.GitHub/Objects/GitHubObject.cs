e41710a8849cb8cd14f558526f24e8985120cd08
Anders Bjerner <abjerner@skybrud.dk>
2015-12-27 14:52:22 +0100

    Introduced the "GitHubObject" class to be used as a base class

---
+++ src/Skybrud.Social.GitHub/Objects/GitHubObject.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Skybrud.Social.GitHub.Objects {
+
+    /// <summary>
+    /// Class representing a basic object from the GitHub API derived from an instance of <code>JObject</code>.
+    /// </summary>
+    public class GitHubObject {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the internal <code>JObject</code> the object was created from.
+        /// </summary>
+        [JsonIgnore]
+        public JObject JObject { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance from the specified <code>obj</code>.
+        /// </summary>
+        /// <param name="obj">The instance of <code>JObject</code> representing the object.</param>
+        protected GitHubObject(JObject obj) {
+            JObject = obj;
+        }
+
+        #endregion
+
+    }
+
+}
