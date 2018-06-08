ec7faa36a07ba2991a56c09886e65921c40dd0a1
Anders Bjerner <abjerner@skybrud.dk>
2018-06-08 19:41:37 +0200

    FIxed wrong date format

--- src/Skybrud.Social.GitHub/Constants/GitHubConstants.cs
+++ src/Skybrud.Social.GitHub/Constants/GitHubConstants.cs
@@ -1,4 +1,6 @@
-﻿namespace Skybrud.Social.GitHub.Constants {
+﻿using Skybrud.Essentials.Time;
+
+namespace Skybrud.Social.GitHub.Constants {
 
     /// <summary>
     /// Static class with various constants used throughout the package.
@@ -8,7 +10,7 @@
         /// <summary>
         /// Gets the datetime format used by the GitHub API.
         /// </summary>
-        public const string DateTimeFormat = "YYYY-MM-DDTHH:MM:SSZ";
+        public const string DateTimeFormat = TimeUtils.Iso8601DateFormat;
 
     }
 
