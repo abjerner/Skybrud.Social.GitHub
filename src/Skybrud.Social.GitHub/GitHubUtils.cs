59cae77cde071f11436f584964660511a1fbe7b6
Anders Bjerner <abjerner@skybrud.dk>
2018-06-08 19:42:57 +0200

    Added new utility methods for formatting timestamps
    As GitHub requires dates to be specified in UTC, the new methods will convert to UTC and then convert to a string using the ISO 8601 date format.

---
+++ src/Skybrud.Social.GitHub/GitHubUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Skybrud.Essentials.Time;
+
+namespace Skybrud.Social.GitHub {
+
+    /// <summary>
+    /// Various utility methods used throughout the GitHub API implementation.
+    /// </summary>
+    public static class GitHubUtils {
+
+        /// <summary>
+        /// Formats the specified <paramref name="timestamp"/> as specified according to the GitHub API documentation.
+        /// </summary>
+        /// <param name="timestamp">The timestamp to be formatted.</param>
+        /// <returns>A string representing <paramref name="timestamp"/>.</returns>
+        public static string FormatDate(DateTime timestamp) {
+            return timestamp.ToUniversalTime().ToString(Constants.GitHubConstants.DateTimeFormat);
+        }
+
+        /// <summary>
+        /// Formats the specified <paramref name="timestamp"/> as specified according to the GitHub API documentation.
+        /// </summary>
+        /// <param name="timestamp">The timestamp to be formatted.</param>
+        /// <returns>A string representing <paramref name="timestamp"/>.</returns>
+        public static string FormatDate(EssentialsDateTime timestamp) {
+            if (timestamp == null) throw new ArgumentNullException(nameof(timestamp));
+            return FormatDate(timestamp.DateTime);
+        }
+
+    }
+
+}
