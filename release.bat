0c944a0aa52cd664dda1551096d8173d00d5f703
Anders Bjerner <abjerner@skybrud.dk>
2019-09-08 22:27:17 +0200

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,4 @@
 @echo off
 cd src/Skybrud.Social.GitHub
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget /p:RunBuildTargets=false
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
 cd ../../
-grunt
