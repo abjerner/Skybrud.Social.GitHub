eaaaad2363b41af613e7bde5e474b3e9e51fea48
Anders Bjerner <abjerner@skybrud.dk>
2019-10-27 00:46:23 +0200

    Introduced the "GitHubListResponse" class

--- src/Skybrud.Social.GitHub/Models/GitHubLinkHeader.cs
+++ src/Skybrud.Social.GitHub/Models/GitHubLinkHeader.cs
@@ -12,42 +12,123 @@ namespace Skybrud.Social.GitHub.Models {
         #region Properties
 
         /// <summary>
+        /// Gets number of the next page.
+        /// </summary>
+        public int Next { get; }
+
+        /// <summary>
         /// Gets the URL of the next page.
         /// </summary>
-        public string Next { get; private set; }
+        public string NextUrl { get; }
+
+        /// <summary>
+        /// Gets whether the list has a next page.
+        /// </summary>
+        public bool HasNextPage => Next > Current;
+
+        /// <summary>
+        /// Gets number of the last page.
+        /// </summary>
+        public int Last { get; }
 
         /// <summary>
         /// Gets the URL of the last page.
         /// </summary>
-        public string Last { get; private set; }
+        public string LastUrl { get; }
+
+        /// <summary>
+        /// Gets number of the first page.
+        /// </summary>
+        public int First { get; }
 
         /// <summary>
         /// Gets the URL of the first page.
         /// </summary>
-        public string First { get; private set; }
+        public string FirstUrl { get; }
+
+        /// <summary>
+        /// Gets the number of the previous page.
+        /// </summary>
+        public int Previous { get; }
+
+        /// <summary>
+        /// Gets whether the list has a previous page.
+        /// </summary>
+        public bool HasPreviousPage => Previous > 1;
 
         /// <summary>
         /// Gets the URL of the previous page.
         /// </summary>
-        public string Previous { get; private set; }
+        public string PreviousUrl { get; }
 
         /// <summary>
         /// Gets the current page number.
         /// </summary>
-        public int CurrentPage { get; private set; }
+        public int Current { get; }
 
         /// <summary>
         /// Gets the total amount of pages.
         /// </summary>
-        public int TotalPages { get; private set; }
+        public int TotalPages { get; }
 
         #endregion
 
         #region Constructors
 
-        private GitHubLinkHeader() {
-            CurrentPage = 1;
+        private GitHubLinkHeader(string value) {
+
+            First = 1;
+            Current = 1;
             TotalPages = 1;
+
+            if (string.IsNullOrWhiteSpace(value)) return;
+
+            // Match the different URLs using REGEX
+            foreach (Match match in Regex.Matches(value, "\\<(.+?)\\>; rel=\"([a-z]+)\"")) {
+
+                string url = match.Groups[1].Value;
+                string rel = match.Groups[2].Value;
+
+                // Match the page parameter from the query string
+                Match m2 = Regex.Match(url, "&page=([0-9]+)");
+
+                // Skip the URL if a page number wasn't part of the URL
+                if (!m2.Success) continue;
+
+                // Parse the page number
+                int page = int.Parse(m2.Groups[1].Value);
+
+                switch (rel) {
+
+                    case "first":
+                        FirstUrl = url;
+                        break;
+
+                    case "prev":
+                        Previous = page;
+                        PreviousUrl = url;
+                        Current = page + 1;
+                        break;
+
+                    case "next":
+                        Next = page;
+                        NextUrl = url;
+                        Current = page - 1;
+                        break;
+
+                    case "last":
+                        Last = page;
+                        LastUrl = url;
+                        TotalPages = page;
+                        break;
+
+                }
+
+            }
+
+            // If we're at the last page, the "last" link is not included
+            TotalPages = Math.Max(TotalPages, Current);
+
         }
 
         #endregion
@@ -59,67 +140,7 @@ namespace Skybrud.Social.GitHub.Models {
         /// <returns>An instance of <see cref="GitHubLinkHeader"/>.</returns>
         public static GitHubLinkHeader Parse(IHttpResponse response) {
             if (response == null) throw new ArgumentNullException(nameof(response));
-            return Parse(response.Headers["Link"]);
-        }
-
-        /// <summary>
-        /// Parses the specified <c>Link</c> header <paramref name="value"/>.
-        /// </summary>
-        /// <param name="value">The header value.</param>
-        /// <returns>An instance of <see cref="GitHubLinkHeader"/>.</returns>
-        public static GitHubLinkHeader Parse(string value) {
-
-            // Return now if the header wasn't present
-            if (string.IsNullOrWhiteSpace(value)) return null;
-
-            // Initialize a new instance with default values
-            GitHubLinkHeader link = new GitHubLinkHeader();
-
-            // Match the different URLs using REGEX
-            foreach (Match match in Regex.Matches(value, "\\<(.+?)\\>; rel=\"([a-z]+)\"")) {
-
-                string url = match.Groups[1].Value;
-                string rel = match.Groups[2].Value;
-
-                // Match the page parameter from the query string
-                Match m2 = Regex.Match(url, "page=([0-9]+)");
-
-                // Skip the URL if a page number wasn't part of the URL
-                if (!m2.Success) continue;
-
-                // Parse the page number
-                int page = int.Parse(m2.Groups[1].Value);
-
-                switch (rel) {
-
-                    case "first":
-                        link.First = url;
-                        break;
-
-                    case "prev":
-                        link.Previous = url;
-                        link.CurrentPage = page + 1;
-                        break;
-
-                    case "next":
-                        link.Next = url;
-                        link.CurrentPage = page - 1;
-                        break;
-
-                    case "last":
-                        link.Last = url;
-                        link.TotalPages = page;
-                        break;
-
-                }
-
-            }
-
-            // If we're at the last page, the "last" link is not included
-            link.TotalPages = Math.Max(link.TotalPages, link.CurrentPage);
-
-            return link;
-
+            return new GitHubLinkHeader(response.Headers["Link"]);
         }
 
     }
---
+++ src/Skybrud.Social.GitHub/Responses/GitHubListResponse.cs
@@ -0,0 +1,39 @@
+﻿using Skybrud.Essentials.Http;
+using Skybrud.Social.GitHub.Models;
+
+namespace Skybrud.Social.GitHub.Responses {
+    
+    /// <summary>
+    /// Class representing a response from the GitHub API.
+    /// </summary>
+    public class GitHubListResponse<T> : GitHubResponse {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a reference to the <c>Link</c> header of the response.
+        /// </summary>
+        public GitHubLinkHeader Link { get; }
+
+        /// <summary>
+        /// Gets the body of the response.
+        /// </summary>
+        public T[] Body { get; protected set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance from the specified <paramref name="response"/>.
+        /// </summary>
+        /// <param name="response">The raw response the instance should be based on.</param>
+        protected GitHubListResponse(IHttpResponse response) : base(response) {
+            Link = GitHubLinkHeader.Parse(response);
+        }
+
+        #endregion
+
+    }
+
+}
