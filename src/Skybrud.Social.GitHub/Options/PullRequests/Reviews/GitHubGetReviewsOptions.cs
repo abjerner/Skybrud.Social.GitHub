0d798ba4c093c1e1412ad6579ab7ce80b8db9fca
Anders Bjerner <abjerner@skybrud.dk>
2020-12-11 21:52:03 +0100

    Updated Skybrud.Essentials.Http dependency

--- src/Skybrud.Social.GitHub/Options/PullRequests/Reviews/GitHubGetReviewsOptions.cs
+++ src/Skybrud.Social.GitHub/Options/PullRequests/Reviews/GitHubGetReviewsOptions.cs
@@ -3,7 +3,6 @@ using Skybrud.Essentials.Common;
 using Skybrud.Essentials.Http;
 using Skybrud.Essentials.Http.Collections;
 using Skybrud.Essentials.Http.Options;
-using Skybrud.Social.GitHub.Extensions;
 using Skybrud.Social.GitHub.Http;
 using Skybrud.Social.GitHub.Models.Issues;
 using Skybrud.Social.GitHub.Models.PullRequests;
