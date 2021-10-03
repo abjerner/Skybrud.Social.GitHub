5ce3cff9ae6e53df402c107b2519f5da1051666f
Anders Bjerner <abjerner@skybrud.dk>
2021-10-03 19:02:12 +0200

    Introduced new GitHubHttpRequestOptions class

--- src/Skybrud.Social.GitHub/Extensions/GitHubExtensions.cs
+++ src/Skybrud.Social.GitHub/Extensions/GitHubExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Skybrud.Essentials.Http.Collections;
+using Skybrud.Social.GitHub.Http;
 using System;
+using System.Collections.Generic;
 
 namespace Skybrud.Social.GitHub.Extensions {
     
@@ -16,6 +18,50 @@ namespace Skybrud.Social.GitHub.Extensions {
         /// <param name="value">The enum value.</param>
         public static void AddEnumValue(this IHttpQueryString query, string key, Enum value) {
             query?.Add(key, GitHubUtils.ToString(value));
+        }
+
+        /// <summary>
+        /// Returns the specified enum has a value. The enum is considered having a value when it differs from the enum's default value.
+        /// </summary>
+        /// <typeparam name="T">The enum type.</typeparam>
+        /// <param name="value">The enum value.</param>
+        /// <returns><c>true</c> if the enum has a value; otherwise <c>false</c>.</returns>
+        public static bool HasValue<T>(this T value) where T : Enum {
+            return !Equals(value, default(T));
+        }
+
+        /// <summary>
+        /// Returns wether the specified integer has a value (not equal to <c>0</c>). 
+        /// </summary>
+        /// <param name="value">The integer value to check.</param>
+        /// <returns><c>true</c> if the integer has a value; otherwise <c>false</c>.</returns>
+        public static bool HasValue(this int value) {
+            return value != 0;
+        }
+        
+        /// <summary>
+        /// Adds the specified <paramref name="mediaType"/> to the <strong>Accept</strong> header of the request.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <param name="mediaType">The media type to be added.</param>
+        public static T AddMediaType<T>(this T options, string mediaType) where T : GitHubHttpRequestOptions {
+            if (string.IsNullOrWhiteSpace(mediaType)) throw new ArgumentNullException(nameof(mediaType));
+            if (options != null) {
+                if (options.MediaTypes == null) options.MediaTypes = new List<string>();
+                options.MediaTypes.Add(mediaType);
+            }
+            return options;
+        }
+
+        /// <summary>
+        /// Removes the specified <paramref name="mediaType"/> from the <strong>Accept</strong> header of the request.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <param name="mediaType">The media type to be removed.</param>
+        public static T RemoveMediaType<T>(this T options, string mediaType) where T : GitHubHttpRequestOptions {
+            if (string.IsNullOrWhiteSpace(mediaType)) throw new ArgumentNullException(nameof(mediaType));
+            options?.MediaTypes?.Remove(mediaType);
+            return options;
         }
 
     }
---
+++ src/Skybrud.Social.GitHub/Http/GitHubHttpRequestOptions.cs
@@ -0,0 +1,22 @@
+﻿using Skybrud.Essentials.Http;
+using Skybrud.Essentials.Http.Options;
+using System.Collections.Generic;
+
+namespace Skybrud.Social.GitHub.Http {
+
+    /// <summary>
+    /// Class describing basic options for a HTTP request to the GitHub API.
+    /// </summary>
+    public abstract class GitHubHttpRequestOptions : IHttpRequestOptions {
+
+        /// <summary>
+        /// Gets or sets the media types that should make up the <strong>Accept</strong> header of requests made using this instance.
+        /// </summary>
+        public List<string> MediaTypes = new List<string>();
+
+        /// <inheritdoc />
+        public abstract IHttpRequest GetRequest();
+
+    }
+
+}
