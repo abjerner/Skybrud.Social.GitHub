a7aedb05dbf9860515c82eeb8cfeb1a3b3e5cfc5
Anders Bjerner <abjerner@skybrud.dk>
2022-02-03 20:00:26 +0100

    Removed unused methods

--- src/Skybrud.Social.GitHub/GitHubUtils.cs
+++ src/Skybrud.Social.GitHub/GitHubUtils.cs
@@ -1,7 +1,6 @@
 ﻿using Skybrud.Essentials.Enums;
 using Skybrud.Essentials.Strings.Extensions;
 using System;
-using Skybrud.Essentials.Time;
 using Skybrud.Social.GitHub.Options;
 using Skybrud.Social.GitHub.Options.Organizations.Members;
 using Skybrud.Social.GitHub.Options.Repositories;
@@ -12,26 +11,6 @@ namespace Skybrud.Social.GitHub {
     /// Various utility methods used throughout the GitHub API implementation.
     /// </summary>
     public static class GitHubUtils {
-
-        /// <summary>
-        /// Formats the specified <paramref name="timestamp"/> as specified according to the GitHub API documentation.
-        /// </summary>
-        /// <param name="timestamp">The timestamp to be formatted.</param>
-        /// <returns>A string representing <paramref name="timestamp"/>.</returns>
-        public static string FormatDate(DateTime timestamp) {
-            return timestamp.ToUniversalTime().ToString(Constants.GitHubConstants.DateTimeFormat);
-        }
-
-        /// <summary>
-        /// Formats the specified <paramref name="timestamp"/> as specified according to the GitHub API documentation.
-        /// </summary>
-        /// <param name="timestamp">The timestamp to be formatted.</param>
-        /// <returns>A string representing <paramref name="timestamp"/>.</returns>
-        public static string FormatDate(EssentialsDateTime timestamp) {
-            if (timestamp == null) throw new ArgumentNullException(nameof(timestamp));
-            return FormatDate(timestamp.DateTime);
-        }
-
 
         /// <summary>
         /// Returns a string representation of the specified <paramref name="filter"/>.
