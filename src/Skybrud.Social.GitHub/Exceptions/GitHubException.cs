1b0274e701ee7d81567d30ffa83d9a2b4ac92e58
Anders Bjerner <abj@limbo.works>
2026-08-23 14:40:26 +0200

    Exception classes should also support inner exceptions

--- src/Skybrud.Social.GitHub/Exceptions/GitHubException.cs
+++ src/Skybrud.Social.GitHub/Exceptions/GitHubException.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Skybrud.Social.GitHub.Exceptions; 
+namespace Skybrud.Social.GitHub.Exceptions;
 
 /// <summary>
 /// Class representing a basic GitHub exception.
@@ -12,5 +12,12 @@ public class GitHubException : Exception {
     /// </summary>
     /// <param name="message">The message of the exception.</param>
     public GitHubException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new exception with the specified <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">The message of the exception.</param>
+    /// <param name="innerException">The inner exception, if any.</param>
+    public GitHubException(string message, Exception? innerException) : base(message, innerException) { }
 
 }
--- src/Skybrud.Social.GitHub/Exceptions/GitHubParseException.cs
+++ src/Skybrud.Social.GitHub/Exceptions/GitHubParseException.cs
@@ -1,4 +1,6 @@
-﻿namespace Skybrud.Social.GitHub.Exceptions; 
+﻿using System;
+
+namespace Skybrud.Social.GitHub.Exceptions;
 
 /// <summary>
 /// Class representing an exception occured when parsing data from the GitHub API.
@@ -10,5 +12,12 @@ public class GitHubParseException : GitHubException {
     /// </summary>
     /// <param name="message">The message of the exception.</param>
     public GitHubParseException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new exception with the specified <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">The message of the exception.</param>
+    /// <param name="innerException">The inner exception, if any.</param>
+    public GitHubParseException(string message, Exception? innerException) : base(message, innerException) { }
 
 }
